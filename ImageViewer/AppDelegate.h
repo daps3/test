dbe73f17f0988fad2b9c90f93626fea5df1b4ebc
Прохоров Виталий <vitaly@iricom.ru>
2014-12-04 17:25:40 +0300

    first commit

---
+++ ImageViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageViewer
+//
+//  Created by Dmitriy on 28/11/14.
+//  Copyright (c) 2014 Dmitriy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RWUIControls.framework/Versions/A/Headers/RWUIControls.h
@@ -0,0 +1,15 @@
+//
+//  RWUIControls.h
+//  RWUIControls
+//
+//  Created by Dmitriy on 27/11/14.
+//  Copyright (c) 2014 Dmitriy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// Knob Control
+#import <RWUIControls/RWKnobControl.h>
+
+// RWRibbon
+#import <RWUIControls/RWRibbonView.h>
